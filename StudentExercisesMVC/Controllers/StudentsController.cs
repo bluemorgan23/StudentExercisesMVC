3445aa491dea660506b184fbd6475ba6b38193a1
Chris Morgan <bluemorgan2323@gmail.com>
2019-07-01 13:48:43 -0500

    trying to fix gitignore file

--- StudentExercisesMVC/Controllers/StudentsController.cs
+++ StudentExercisesMVC/Controllers/StudentsController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using StudentExercisesAPI.Models;
+using StudentExercisesMVC.Models;
 
 namespace StudentExercisesMVC.Controllers
 {
