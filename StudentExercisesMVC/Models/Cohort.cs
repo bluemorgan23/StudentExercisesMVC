0771401c8cd3f409a72962c37e8f3b3008725674
Chris Morgan <bluemorgan2323@gmail.com>
2019-07-03 13:31:38 -0500

    Changed display name of CohortName to Cohort Name. Also made this a required attribute along with Id

--- StudentExercisesMVC/Models/Cohort.cs
+++ StudentExercisesMVC/Models/Cohort.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace StudentExercisesMVC.Models
 {
@@ -11,7 +12,11 @@ namespace StudentExercisesMVC.Models
             Id = id;
         }
         */
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Cohort Name")]
         public string CohortName {get; set;}
         public List<Student> ListOfStudents {get; set;} = new List<Student>();
         public List<Instructor> ListOfInstructors {get; set;} = new List<Instructor>();
