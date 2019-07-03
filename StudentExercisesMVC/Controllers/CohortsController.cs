d5c1cd72d08cf9145b4fe051c6ef8ffbb0d6fed0
Chris Morgan <bluemorgan2323@gmail.com>
2019-07-03 13:06:51 -0500

    Created cohort controller and the GetAllCohorts method for the Index() view which is a list of all the cohorts.

---
+++ StudentExercisesMVC/Controllers/CohortsController.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExercisesMVC.Models;
+
+namespace StudentExercisesMVC.Controllers
+{
+    public class CohortsController : Controller
+    {
+
+        private readonly IConfiguration _config;
+
+        public CohortsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // GET: api/Cohort
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT 
+                                        c.Id AS CohortId,
+                                        c.CohortName
+                                        
+                                        FROM Cohort c";
+                                        
+
+                    //s.Id AS StudentId,
+                    //                    s.FirstName AS SFirst,
+                    //                    s.LastName AS SLast,
+                    //                    i.Id AS InstructorId,
+                    //                    i.FirstName AS IFirst,
+                    //                    i.LastName AS ILast
+
+                    //                    LEFT JOIN Student s ON s.CohortId = c.Id
+                    //                    LEFT JOIN Instructor i ON i.CohortId = c.Id";
+
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    //List<Instructor> instructors = new List<Instructor>();
+
+                    //List<Student> students = new List<Student>();
+
+                    List<Cohort> cohorts = new List<Cohort>();
+
+                    while (reader.Read())
+                    {
+                        //Student student = new Student
+                        //{
+                        //    Id = reader.GetInt32(reader.GetOrdinal("StudentId")),
+                        //    FirstName = reader.GetString(reader.GetOrdinal("SFirst")),
+                        //    LastName = reader.GetString(reader.GetOrdinal("SLast")),
+                        //    CohortId = reader.GetInt32(reader.GetOrdinal("CohortId"))
+                        //};
+
+
+                        //students.Add(student);
+
+
+
+
+                        //Instructor instructor = new Instructor
+                        //{
+                        //    Id = reader.GetInt32(reader.GetOrdinal("InstructorId")),
+                        //    FirstName = reader.GetString(reader.GetOrdinal("IFirst")),
+                        //    LastName = reader.GetString(reader.GetOrdinal("ILast")),
+                        //    CohortId = reader.GetInt32(reader.GetOrdinal("CohortId"))
+                        //};
+
+                        //instructors.Add(instructor);
+
+
+                        Cohort cohort = new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("CohortId")),
+                            CohortName = reader.GetString(reader.GetOrdinal("CohortName"))
+                            //ListOfStudents = students.Where(s => s.CohortId == reader.GetInt32(reader.GetOrdinal("CohortId"))).ToList(),
+                            //ListOfInstructors = instructors.Where(i => i.CohortId == reader.GetInt32(reader.GetOrdinal("CohortId"))).ToList()
+                        };
+
+                        cohorts.Add(cohort);
+
+                        //if (cohorts.Any(c => c.Id == cohort.Id))
+                        //{
+                        //    cohort.ListOfInstructors.Add(instructor);
+
+                        //    cohort.ListOfStudents.Add(student);
+
+                        //    Cohort cohortToUpdate = cohorts.Find(c => c.Id == cohort.Id);
+                        //    if (!cohortToUpdate.ListOfInstructors.Any(i => i.Id == instructor.Id))
+                        //    {
+                        //        cohortToUpdate.ListOfInstructors.Add(instructor);
+                        //    }
+
+                        //    if (!cohortToUpdate.ListOfStudents.Any(s => s.Id == student.Id))
+                        //    {
+                        //        cohortToUpdate.ListOfStudents.Add(student);
+                        //    }
+
+                        //}
+                        //else
+                        //{
+                        //    cohorts.Add(cohort);
+                        //}
+
+                    }
+
+                    reader.Close();
+
+                    return View(cohorts);
+                }
+            }
+
+
+        }
+
+        // GET: Cohorts/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cohorts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cohorts/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cohorts/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cohorts/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cohorts/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cohorts/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
